408aa1d9f777b9afdea471a4c02c422dc859f8f8
Steve Oziel <gmoz22@gmail.com>
2017-06-08 17:48:25 -0700

    Fix install script

--- daemon.sh
+++ daemon.sh
@@ -1,3 +1,3 @@
 #bin/bash!
 
-pm2 start start.js -o pm2_output.log -e pm2_errors.log -- -e token,rand -b 404,500 -d ./data
+pm2 start start.js -o pm2_output.log -e pm2_errors.log -- -e token,rand -b 404,500 -d /var/www/html/caching-proxy/data
--- install-as-daemon.sh
+++ install-as-daemon.sh
@@ -19,18 +19,21 @@ sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable;
 
+# Install PM2 and kill existing pm2 processes if any
+sudo npm install -g pm2
+pm2 kill
+
 npm install caching-proxy;
 
 echo '#!/bin/bash' > /var/www/html/caching-proxy/daemon.sh;
 printf '\n\n' >> /var/www/html/caching-proxy/daemon.sh;
 echo 'cd /var/www/html/caching-proxy/node_modules/caching-proxy' >> /var/www/html/caching-proxy/daemon.sh;
-echo 'nohup ./daemon.sh -e token,rand -d /var/www/html/caching-proxy/data &' >> /var/www/html/caching-proxy/daemon.sh;
+echo 'pm2 start start.js -o pm2_output.log -e pm2_errors.log -- -e token,rand -b 404,500 -d /var/www/html/caching-proxy/data' >> /var/www/html/caching-proxy/daemon.sh;
 
 chmod 777 /var/www/html/caching-proxy/daemon.sh;
 
 echo '/var/www/html/caching-proxy/daemon.sh' >> /etc/rc.local;
 
-#GET DATA
-rm -rf /var/www/html/caching-proxy/data;
-
 /var/www/html/caching-proxy/daemon.sh
+
+pm2 list
